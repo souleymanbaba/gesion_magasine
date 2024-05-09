f17860a576c0c133ab97c191dba93d59dbba97cf
22072 22072 <22072@supnum.mr>
2024-05-09 14:20:37 +0100

    stage

---
+++ stage/e_commerce/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Example from './customer/components/Navigation/Navigation';
+import Navigation from './customer/components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="">
+      <div>
+        <Navigation/>
+       
+
+        
+      </div>
+      home
+    
+    </div>
+  );
+}
+
+export default App;
